81fbcc286513f4fa988e2fa4e322d0a8736f7da9
xudongweng <527924966@qq.com>
2020-05-21 17:45:59 +0800

    2020-05-21 17:45

--- DataAnt/src/main/java/com/dataant/TransferData.java
+++ DataAnt/src/main/java/com/dataant/TransferData.java
@@ -7,18 +7,12 @@ package com.dataant;
 
 import com.dataant.controller.ComposeSQL;
 import com.dataant.controller.ExecuteSQLController;
+import com.dataant.controller.LoadConfigFile;
 import com.dataant.model.LoadMySQLProperity;
 import com.dataant.model.LoadTableProperity;
 import com.dataant.helper.MySQLHelper;
-import com.dataant.model.DTableObject;
-import com.dataant.model.MySQLObject;
-import com.dataant.model.STableObject;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Queue;
 import org.apache.log4j.Logger;
 
@@ -29,70 +23,46 @@ import org.apache.log4j.Logger;
 public class TransferData {
     public static void main(String[] args){
         Logger log=Logger.getLogger(TransferData.class);
-        try{
-            File file = new File("config.properties");
-            if(!file.exists()){
-                log.error("File "+file.getAbsolutePath()+"config.properties is not exist.");
-                return;
-            }
-            //加载配置
-            Properties prop = new Properties();
-            prop.load(new FileInputStream("config.properties"));
 
-            MySQLObject mysqlobj=new MySQLObject(prop.getProperty("host"),prop.getProperty("user"),
-                    prop.getProperty("password"),prop.getProperty("port"),prop.getProperty("characterEncoding"));
-            LoadMySQLProperity.setMySQLObj(mysqlobj);
-            
-            DTableObject dobj=new DTableObject(prop.getProperty("dDB"),prop.getProperty("dTable"),prop.getProperty("dCol"));
-            STableObject sobj=new STableObject(prop.getProperty("sDB"),prop.getProperty("sTable"),prop.getProperty("sCol"));
-            switch(LoadTableProperity.setTable(dobj, sobj)){
-                case -1:log.error("dDB is empty."); return;
-                case -2:log.error("dTable is empty."); return;
-                case -3:log.error("dCol is empty."); return;
-                case -4:log.error("sDB is empty."); return;
-                case -5:log.error("sTable is empty."); return;
-                case -6:log.error("sCol is empty."); return;
-                case -7:log.error("dCol can't contain '(' or ')'."); return;
-                case -8:log.error("dCol is inconformity with sCol."); return;
-            }
-            LoadTableProperity.setSqlType(Integer.parseInt(prop.getProperty("sqlType")));
-            LoadTableProperity.setPerLimit(Integer.parseInt(prop.getProperty("limit")));
-            
-            MySQLHelper mysqlhelper=new MySQLHelper();
-            mysqlhelper.setURL(mysqlobj.getHost(), mysqlobj.getPort(), mysqlobj.getUser(), mysqlobj.getPassword(),mysqlobj.getCharacterEncoding());
-            List cols=mysqlhelper.queryAll("SHOW COLUMNS FROM "+sobj.getSDB()+"."+sobj.getSTable());
-            if (cols==null) return;
-            
-            for(int i=0;i<cols.size();i++){
-                //System.out.println(cols.get(i));
-                Map<String,Object> tabledesc=(Map<String,Object>) cols.get(i);
-                //System.out.println(tabledesc.get("Key"));
-                if(tabledesc.get("Key").equals("PRI")){
-                    if(tabledesc.get("Type").toString().contains("int")){
-                        LoadTableProperity.setPK(tabledesc.get("Field").toString());
-                        //System.out.println(LoadTableProperity.getPK());
-                        break;
-                    }else{
-                        log.error("Table "+sobj.getSTable()+" doesn't exist primary key of Integer.");
-                        return;
-                    }
+        LoadConfigFile lcf=new LoadConfigFile();
+        if(lcf.loadFile()==0)return;
+        if(lcf.setMySQLData()==0)return;
+        lcf.setSQLData();
+
+
+        MySQLHelper mysqlhelper=new MySQLHelper();
+        mysqlhelper.setURL(LoadMySQLProperity.getMySQLObj().getHost(), LoadMySQLProperity.getMySQLObj().getPort(), LoadMySQLProperity.getMySQLObj().getUser()
+                , LoadMySQLProperity.getMySQLObj().getPassword(),LoadMySQLProperity.getMySQLObj().getCharacterEncoding());
+        List cols=mysqlhelper.queryAll("SHOW COLUMNS FROM "+LoadTableProperity.getST().getSDB()+"."+LoadTableProperity.getST().getSTable());
+        if (cols==null) return;
+
+        for(int i=0;i<cols.size();i++){
+            //System.out.println(cols.get(i));
+            Map<String,Object> tabledesc=(Map<String,Object>) cols.get(i);
+            //System.out.println(tabledesc.get("Key"));
+            if(tabledesc.get("Key").equals("PRI")){
+                if(tabledesc.get("Type").toString().contains("int")){
+                    LoadTableProperity.setPK(tabledesc.get("Field").toString());
+                    //System.out.println(LoadTableProperity.getPK());
+                    break;
+                }else{
+                    log.error("Table "+LoadTableProperity.getST().getSTable()+" doesn't exist primary key of Integer.");
+                    return;
                 }
             }
-            if(LoadTableProperity.getPK().equals("")){
-                log.error("Table "+sobj.getSTable()+" doesn't exist primary key.");
-                return;
-            }
-            List range=mysqlhelper.queryAll("SELECT MAX("+LoadTableProperity.getPK() +") AS max,MIN("+LoadTableProperity.getPK()+") AS min FROM "+sobj.getSDB()+"."+sobj.getSTable());
-            if(range==null) return;
-            Map<String,Object> idrange=(Map<String,Object>) range.get(0);
-            //idrange.get("max")
-            ComposeSQL csql=new ComposeSQL(Integer.parseInt(idrange.get("max").toString()),Integer.parseInt(idrange.get("min").toString()));
-            csql.construct(LoadTableProperity.getSqlType());
-            Queue<String> sqlQueue=csql.getSQLQueue();
-            ExecuteSQLController exSQL=new ExecuteSQLController();
-            exSQL.execute(sqlQueue);
-        }catch(IOException e){
-            log.error(e.toString());
         }
+        if(LoadTableProperity.getPK().equals("")){
+            log.error("Table "+LoadTableProperity.getST().getSTable()+" doesn't exist primary key.");
+            return;
+        }
+        List range=mysqlhelper.queryAll("SELECT MAX("+LoadTableProperity.getPK() +") AS max,MIN("+LoadTableProperity.getPK()+") AS min FROM "+LoadTableProperity.getST().getSDB()+"."+LoadTableProperity.getST().getSTable());
+        if(range==null) return;
+        Map<String,Object> idrange=(Map<String,Object>) range.get(0);
+        //idrange.get("max")
+        ComposeSQL csql=new ComposeSQL(Integer.parseInt(idrange.get("max").toString()),Integer.parseInt(idrange.get("min").toString()));
+        csql.construct(LoadTableProperity.getSqlType());
+        Queue<String> sqlQueue=csql.getSQLQueue();
+        ExecuteSQLController exSQL=new ExecuteSQLController();
+        exSQL.execute(sqlQueue);
     }
 }
--- DataAnt/src/main/java/com/dataant/controller/ExecuteSQLController.java
+++ DataAnt/src/main/java/com/dataant/controller/ExecuteSQLController.java
@@ -8,13 +8,8 @@ package com.dataant.controller;
 import com.dataant.helper.MySQLHelper;
 import com.dataant.model.ExecuteObject;
 import com.dataant.model.LoadMySQLProperity;
-import com.dataant.model.LoadTableProperity;
 import com.dataant.model.MySQLObject;
-import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import org.apache.log4j.Logger;
 
 /**
@@ -35,13 +30,18 @@ public class ExecuteSQLController {
         mysqlhelper.setURL(mysqlobj.getHost(), mysqlobj.getPort(), mysqlobj.getUser(), mysqlobj.getPassword(),mysqlobj.getCharacterEncoding());
         LoadMySQLProperity.setMaxLine(sqlQueue.size());
         LoadMySQLProperity.setMinLine(sqlQueue.size());
-        int i=sqlQueue.size();
+
         
         while(sqlQueue.size()>0){
-            String sql=sqlQueue.poll();
+            String sql=sqlQueue.poll();//每次取出第一条SQL，并删除取出的第一条SQL.
+            java.sql.Date currentDate1 = new java.sql.Date(System.currentTimeMillis()); //计算执行时间
+            long longtime1=currentDate1.getTime();
             log.info(" execute startup -- "+sql);
             boolean bool=mysqlhelper.execSQL(sql);
             log.info(" execute finished -- "+sql);
+            java.sql.Date currentDate2 = new java.sql.Date(System.currentTimeMillis()); 
+            long longtime2=currentDate2.getTime();
+            System.out.println("remain -- "+String.valueOf((longtime2-longtime1)*(sqlQueue.size()-1)/1000) + "s");//显示预计执行所需要的时间
             if(bool){
                 LoadMySQLProperity.setMinLine(LoadMySQLProperity.getMinLine()-1);
                 this.execobj.setNumber(LoadMySQLProperity.getMaxLine(), LoadMySQLProperity.getMinLine());
